7036a5d9f29a738d7bb93fdeaa3b6da8f5be3d7a
vipr728 <vipr728@gmail.com>
2025-07-09 16:51:48 -0700

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 
 export default function Home() {
 	return (
-		<div className="  h-dvh w-dvw flex flex-col justify-center items-center">
-			<div className="flex flex-col justify-center items-start p-4">
-				<h1 className="text-2xl italic mb-2">Hi, im</h1>
-				<h1 className="font-extrabold text-9xl italic opacity-0 translate-x-[-100%] animate-slideIn">abhi</h1>
+		<div className="h-dvh w-dvw flex flex-col bg-black text-white">
+			<header className="w-full p-4 flex justify-center items-center">
+				<nav className="flex gap-4 underline">
+					<a href="#projects">Projects</a>
+					<a href="#contact">Contact</a>
+					<a href="https://www.linkedin.com/in/abhinav-bandaru-a768022a5/" target="_blank">LinkedIn</a>
+					<a href="https://github.com/vipr728" target="_blank">GitHub</a>
+				</nav>
+			</header>
+			<div className="flex flex-col justify-center items-center flex-grow">
+				<div className="flex flex-col justify-center items-start p-4">
+					<h1 className="text-2xl italic mb-2">Hi, im</h1>
+					<h1 className="font-extrabold text-9xl italic opacity-0 translate-x-[-100%] animate-slideIn">abhi</h1>
+				</div>
 			</div>
 			<footer className="absolute bottom-0 left-0 w-full p-4 text-center">
 				<p className="text-sm text-gray-500">© 2025 Abhinav Bandaru. All rights reserved.</p>
